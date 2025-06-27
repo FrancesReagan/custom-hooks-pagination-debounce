a2296b7e14811588a119115f971db0f42a675430
Frances Reagan James <adtgwen@gmail.com>
2025-06-27 07:10:39 -0400

    added fixes to debounce page

--- src/components/DebounceSearch.jsx
+++ src/components/DebounceSearch.jsx
@@ -1,1 +1,41 @@
+import { useState, useEffect, use } from "react";
+import useDebounce from "../hooks/useDebounce";
 
+function DebounceSearch() {
+  const [inputValue, setInputValue] = useState("");
+  const [delay, setDelay] = useState(500);
+  const debouncedValue = useDebounce(inputValue, delay);
+
+  // simulate API call//
+  useEffect(() => {
+    if (debouncedValue) {
+      console.log(`Searching for: ${debouncedValue}`);
+    }
+  },[debouncedValue]);
+
+  return (
+    <div>
+      <h2>Debounce Search</h2>
+      <label>
+        Debounce Delay(ms):{""}
+        <input
+          type="number"
+          value-{delay}
+          onChange={(e) => setDelay(Number(e.target.value))}
+          min="0"
+          />
+      </label>
+      <br/>
+      <label>
+        Type to search:{""}
+        <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        />
+      </label>
+      <p>Current Input:{inputValue}</p>
+      <p>Debounced Value (after {delay}ms):{debouncedValue}</p>
+    </div>
+  )
+}
